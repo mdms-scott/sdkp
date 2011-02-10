bd2b88592964d78b8791ba8ee6353735c24c1fb1
mdms-scott <mdms.scott@gmail.com>
2011-02-10 15:36:27 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+
+gem 'mysql2'
+
+gem 'haml'
+gem 'annotate-models'
+gem 'rspec'
+gem 'rspec-rails'
+gem 'cucumber'
+gem 'cucumber-rails'
+gem 'spork'
+gem 'will_paginate', '>=3.0.pre'
+gem 'autotest'
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sdkp::Application.config.secret_token = 'e9363d2efa117e8bad6cc11575df2661cef7ba97883440ed2e1500dd9fcb40ba9f5f68941b6d72744db771c61e4187f3deeff30ecf6d699acddd872b7058c07c'
